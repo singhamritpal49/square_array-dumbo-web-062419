37996bd418c46120f8d4fcd7c6fc9f8c4c3592f5
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-01 04:06:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
   new_array = []
-array.each do |number|new_array.push(numbers*numbers) 
-  numbers
+  array.each{|numbers| new_array.push(numbers*numbers)}
+  return new_array
 end
