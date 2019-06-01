df9099c0c8ec084595dc27d58272d2010eddeae4
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-01 03:41:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,5 @@
 def square_array(array)
-  array.each do |number|
-    new_number = number ** 2
-    new_array = []
-    new_array.push (new_number)
-    return new_array
-  end
+array.each do |i|
+  puts i
+end
 end
